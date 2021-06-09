d822e5721ed0253b5ac857c28b4b3ea4e8e5151e
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-09 18:51:30 -0500

    [Feature]: New user - welcome email

--- worker.js
+++ worker.js
@@ -5,6 +5,7 @@ import fs from 'fs';
 import dbClient from './utils/db';
 
 const fileQueue = new Queue('fileQueue');
+const userQueue = new Queue('userQueue');
 
 const generateThumbnail = async (file, options) => {
   try {
@@ -40,6 +41,24 @@ fileQueue.process(async (job, done) => {
   generateThumbnail(file.localPath, { width: 500 });
   generateThumbnail(file.localPath, { width: 250 });
   generateThumbnail(file.localPath, { width: 100 });
+
+  done();
+});
+
+userQueue.process(async (job, done) => {
+  const { userId } = job.data;
+  if (!userId) {
+    throw Error('Missing userId');
+  }
+
+  const user = await dbClient.db.collection('users').findOne({
+    _id: ObjectId(userId),
+  });
+  if (!user) {
+    throw Error('User not found');
+  }
+
+  console.log(`Welcome ${user.email}`);
 
   done();
 });
